6ee762c5893ae9178bd82d3f70b058ca0eb2f3ee
Yuri Tokio <yuri@sheild.com.br>
2023-07-03 20:23:37 -0300

    tudo ok

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,16 +1,17 @@
-import "../styles/components/socialnetworkcontainer.sass";
+import "../styles/components/aboutContainer.sass"
 
 const AboutContainer = () => {
   return (
-    <section className="about-container">
-      <h2 className="h5">Apresentação</h2>
-      <p>
-        Comecei minha carreira de desenvolvedor na Tok&Stok como Desenvolvedor WEB e durante meu aprendizado, desenvolvi habilidades valiosas e estou confiante de que posso aplicar esses conhecimentos para contribuir com sucesso em projetos desafiadores.
-      </p> 
-      <p>
-        Também sou uma pessoa proativa, orientada para resultados e tenho uma mente analítica. Acredito que sou capaz enfrentar os desafios do setor e a contribuir significativamente para a equipe.
-      </p>
-      
+    <section id="about-container">
+      <div className="about">
+        <h2>Apresentação</h2>
+        <p>
+          Comecei minha carreira de desenvolvedor na Tok&Stok como Desenvolvedor WEB e durante meu aprendizado, desenvolvi habilidades valiosas e estou confiante de que posso aplicar esses conhecimentos para contribuir com sucesso em projetos desafiadores.
+        </p>
+        <p>
+          Também sou uma pessoa proativa, orientada para resultados e tenho uma mente analítica. Acredito que sou capaz enfrentar os desafios do setor e a contribuir significativamente para a equipe.
+        </p>
+      </div>
     </section>
   );
 };
