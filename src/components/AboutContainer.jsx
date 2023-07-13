3d1f899e657bca829e585048e4c39cf3b9fd5c29
Yuri Tokio <yuri@sheild.com.br>
2023-07-13 13:09:54 -0300

    update

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,7 +6,7 @@ const AboutContainer = () => {
       <div className="about">
         <h2>Apresentação</h2>
         <p>
-          Com um pouco mais de 2 anos de experiência em Desenvolvimento Web, trabalhei com <span>ASP.NET, Node Js, SASS, React, Postman e GIT</span> para criar soluções robustas e escaláveis que atendam às necessidades dos clientes. 
+          Com um pouco mais de 2 anos de experiência em Desenvolvimento Web, sou capaz de criar soluções robustas e escaláveis utilizando ASP.NET, Node.js, SASS, React, Postman e GIT, visando atender às necessidades dos clientes e contribuir com sucesso em projetos desafiadores.
         </p>
         <p>
           Também sou uma pessoa proativa, orientada para resultados e tenho uma mente analítica. Acredito que sou capaz enfrentar os desafios do setor e a contribuir significativamente para a equipe.
