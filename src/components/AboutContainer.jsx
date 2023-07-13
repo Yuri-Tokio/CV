dcf6da8360908f8a4d41146c196b5aedbca2585a
Yuri Tokio <yuri@sheild.com.br>
2023-07-13 12:57:48 -0300

    update

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,7 +6,7 @@ const AboutContainer = () => {
       <div className="about">
         <h2>Apresentação</h2>
         <p>
-          Comecei minha carreira de desenvolvedor na Tok&Stok como Desenvolvedor WEB e durante meu aprendizado, desenvolvi habilidades valiosas e estou confiante de que posso aplicar esses conhecimentos para contribuir com sucesso em projetos desafiadores.
+          Com um pouco mais de 2 anos de experiência em Desenvolvimento Web, trabalhei com <span>ASP.NET, Node Js, SASS, React, Postman e GIT</span> para criar soluções robustas e escaláveis que atendam às necessidades dos clientes. 
         </p>
         <p>
           Também sou uma pessoa proativa, orientada para resultados e tenho uma mente analítica. Acredito que sou capaz enfrentar os desafios do setor e a contribuir significativamente para a equipe.
